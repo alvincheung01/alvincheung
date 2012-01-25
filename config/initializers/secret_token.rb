cf21ca055370a9ca7e2ae30d22212ff0791d58fa
Alvin Cheung <alvincheung01@gmail.com>
2012-01-25 13:40:00 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlvinCheung::Application.config.secret_token = '9179a1e813c4b03d3efd76cb2ecc4a18d48df7a45310386169b6f0cfbab101173f3551b9c6b4dd7280e355e2132038c85cc2e9cf9769697c9de761d3c71ddf68'
